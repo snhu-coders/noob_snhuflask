5bed962c7f5505fd018b61fceb9029dcc133ecf2
JD Thomason <joseph.thomason@snhu.edu>
2019-05-22 18:58:02 -0700

    Set up config loader

--- noob_snhuflask.py
+++ noob_snhuflask.py
@@ -7,7 +7,28 @@ Be warned.  I have no idea what I'm doing.
 
 # Import some stuff here.
 
+
+import sys
+import yaml
 from flask import Flask, jsonify
+
+# First things first, load the config:
+
+try:
+    with open("config.yml", 'r') as file:
+        print("Attempting to load config.yml")
+        CONFIG = yaml.load(file.read(), Loader=yaml.BaseLoader)
+        FLASK_CONFIG = CONFIG["slackflask"]
+        """
+        Values available via:
+
+        FLASK_CONFIG["token"] for the usual bot token
+        FLASK_CONFIG["oauth"] for the oauth token
+        FLASK_CONFIG["sign-secret"] for the Slack signing secret
+        """
+except FileNotFoundError as err:
+    print("Could not load config file.  Exiting.")
+    sys.exit()
 
 app = Flask(__name__)
 
